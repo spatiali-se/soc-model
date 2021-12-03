99ff570a68dc928bce5f82b3d6b14e3665818d8c
nmucke <nmucke@gmail.com>
2021-12-03 09:29:25 +0100

    feat: added feature and target transform to SoilDataset class

--- modules/datasets.py
+++ modules/datasets.py
@@ -4,15 +4,21 @@ import torch
 class SoilDataset(torch.utils.data.TensorDataset):
     """Soil Features dataset."""
 
-    def __init__(self, data):
+    def __init__(self, data, features_transform=None, target_transform=None):
         super().__init__()
         """
         Args:
             data (pandas DataFrame): The pandas DataFrame containing the targets and features.
-            transform (callable, optional): Optional transform to be applied on a sample.
+            feature_transform (callable, optional): Optional transform to be applied on a feature sample.
+            target_transform (callable, optional): Optional transform to be applied on a target sample.
         """
         self.targets = data[:, 0]
         self.features = data[:, 1:]
+
+        if features_transform is not None:
+            self.features_transform = features_transform
+        if target_transform is not None:
+            self.target_transform = target_transform
 
     def __len__(self):
         """Get length of dataset."""
@@ -24,4 +30,10 @@ class SoilDataset(torch.utils.data.TensorDataset):
             idx = idx.tolist()
         targ = self.targets[idx]
         feat = self.features[idx]
+
+        if self.features_transform is not None:
+            feat = self.features_transform(feat)
+        if self.target_transform is not None:
+            targ = self.target_transform(targ)
+
         return {"target": targ, "features": feat}
