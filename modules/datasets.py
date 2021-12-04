1ec3080b067a910d4a540e96a2f82fa3e8fe22ed
Peter Piontek <piontek0@gmail.com>
2021-12-04 20:41:53 +0100

    feat: added preprocessing and no val set option

--- modules/datasets.py
+++ modules/datasets.py
@@ -1,6 +1,7 @@
 import torch
 from sklearn.model_selection import train_test_split
-import pdb
+from torch.utils.data.dataloader import DataLoader
+
 
 class SoilDataset(torch.utils.data.TensorDataset):
     """Soil Features dataset."""
@@ -35,26 +36,34 @@ class SoilDataset(torch.utils.data.TensorDataset):
         if self.target_transform:
             targ = self.target_transform(targ)
 
-        return {"target": targ, "features": feat}
+        return {"features": feat, "target": targ}
 
-def split_data(data, split):
-    """Splits dataset to train, val, and test data"""
 
-    train_ratio = split[0]
-    val_ratio = split[1]
-    test_ratio = split[2]
+def split_data(data, train_ratio, test_ratio):
+    """Splits dataset into train, val, and test data"""
 
-    train_data, test_data = train_test_split(data, test_size=1 - train_ratio)
+    # Split into train(-val) and test
+    train_split, test_split = train_test_split(data, test_size=test_ratio)
+    # Split train(-val) into train and val if wanted
+    if train_ratio + test_ratio == 1:
+        return train_split, test_split
 
-    val_data, test_data = train_test_split(test_data,
-                                           test_size=test_ratio / (
-                                                       test_ratio + val_ratio))
-    return train_data, val_data, test_data
+    train_split, val_split = train_test_split(
+        train_split, test_size=1 - train_ratio - test_ratio
+    )
 
-def get_data_loaders(data, split=[0.7, 0.15, 0.15],
-                     batch_size=64,
-                     features_transform=None,
-                     target_transform=None):
+    return train_split, val_split, test_split
+
+
+def get_data_loaders(
+    data,
+    train_ratio=0.7,
+    test_ratio=0.15,
+    batch_size=64,
+    preprocessor=None,
+    features_transform=None,
+    target_transform=None,
+):
     """Generates train, val, and test dataloaders
 
     args:
@@ -63,30 +72,49 @@ def get_data_loaders(data, split=[0.7, 0.15, 0.15],
         feature_transform (callable, optional): Optional transform to be applied on a feature sample.
         target_transform (callable, optional): Optional transform to be applied on a target sample.
     """
+    # TODO: num_workers on batchloaders?
 
     # Split data
-    train_data, val_data, test_data = split_data(data=data, split=split)
+    train_data, *test_data = split_data(
+        data=data, train_ratio=train_ratio, test_ratio=test_ratio
+    )
+
+    if len(test_data) == 2:
+        val_data, test_data = test_data
+
+    # Preprocess data
+    if preprocessor != None:
+        train_data = preprocessor.fit_transform(train_data)
+        test_data = preprocessor.transform(test_data)
+        if "val_data" in locals():
+            val_data = preprocessor.transform(val_data)
 
     # Train dataset
-    train_data = SoilDataset(train_data,
-                             features_transform=features_transform,
-                             target_transform=target_transform)
-    # Val dataset
-    val_data = SoilDataset(val_data,
-                           features_transform=features_transform,
-                           target_transform=target_transform)
+    train_data = SoilDataset(
+        train_data,
+        features_transform=features_transform,
+        target_transform=target_transform,
+    )
+
     # Test dataset
     test_data = SoilDataset(test_data)
 
     # Train dataloader
-    train_dataloader =torch.utils.data.DataLoader(train_data,
-                                                  batch_size=batch_size,
-                                                  shuffle=True)
-    # Val dataloader
-    val_dataloader =torch.utils.data.DataLoader(val_data,
-                                                batch_size=batch_size)
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     # Test dataloader
-    test_dataloader =torch.utils.data.DataLoader(test_data,
-                                                batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-    return train_dataloader, val_dataloader, test_dataloader
+    # Check for val dataset
+    if "val_data" in locals():
+        # Val dataset
+        val_data = SoilDataset(
+            val_data,
+            features_transform=features_transform,
+            target_transform=target_transform,
+        )
+        # Val dataloader
+        val_dataloader = DataLoader(val_data, batch_size=batch_size)
+
+        return train_dataloader, val_dataloader, test_dataloader
+
+    return train_dataloader, test_dataloader
