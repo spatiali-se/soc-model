318b0074a91b4609ff205f18300f9b065bb7cef4
Peter Piontek <piontek0@gmail.com>
2021-12-05 01:33:54 +0100

    feat: added mlflow logging

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.optim as optim
 import torch
+import mlflow
 
 from modules.datasets import get_data_loaders
 from modules.training import Trainer
@@ -99,39 +100,51 @@ if __name__ == "__main__":
     train_params = {"num_epochs": 10, "patience": 10, "early_stopping": True}
     optim_params = {"lr": 1e-2, "weight_decay": 1e-6}
 
-    # Create NN
-    model = models.DenseNN(**nn_params)
-    # Set up optimizer
-    optimizer = optim.Adam(params=model.parameters(), **optim_params)
-    # Set up loss function
-    loss_function = nn.MSELoss()
-    # Set up val metrics
-    val_metrics = [nn.MSELoss(), nn.L1Loss()]
+    with mlflow.start_run():
+        # Create NN
+        model = models.DenseNN(**nn_params)
+        # Set up optimizer
+        optimizer = optim.Adam(params=model.parameters(), **optim_params)
+        # Set up loss function
+        loss_function = nn.MSELoss()
+        # Set up val metrics
+        val_metrics = [nn.MSELoss(), nn.L1Loss()]
 
-    # Set up NN trainer
-    trainer = Trainer(
-        model=model, optimizer=optimizer, loss_function=loss_function, device=device
-    )
-    train_loss, val_metrics = trainer.fit(
-        train_loader=train_dataloader,
-        val_loader=val_dataloader,  # Currently
-        val_metrics=val_metrics,
-        **train_params,
-    )
+        # Set up NN trainer
+        trainer = Trainer(
+            model=model, optimizer=optimizer, loss_function=loss_function, device=device
+        )
+        train_loss, val_metrics = trainer.fit(
+            train_loader=train_dataloader,
+            val_loader=val_dataloader,  # Currently
+            val_metrics=val_metrics,
+            **train_params,
+        )
 
-    # Plot training metrics
-    if plot_metrics:
-        plt.figure()
-        plt.semilogy(train_loss, linewidth=2.0, label="Train MSE loss")
-        plt.semilogy(val_metrics[:, 0], linewidth=2.0, label="Val MSE loss")
-        plt.semilogy(val_metrics[:, 1], linewidth=2.0, label="Val L1 loss")
-        plt.grid()
-        plt.legend()
-        plt.show()
+        # Plot training metrics
+        if plot_metrics:
+            plt.figure()
+            plt.semilogy(train_loss, linewidth=2.0, label="Train MSE loss")
+            plt.semilogy(val_metrics[:, 0], linewidth=2.0, label="Val MSE loss")
+            plt.semilogy(val_metrics[:, 1], linewidth=2.0, label="Val L1 loss")
+            plt.grid()
+            plt.legend()
+            plt.show()
 
-    # Run test routine
-    test_model = test_routines.Tester(model, test_dataloader)
+        # Run test routine
+        test_model = test_routines.Tester(model, test_dataloader)
 
-    # Print test metrics
-    if print_metrics:
-        test_model.print_metrics()
+        # TODO: Fix why it's printing metrics when args is set to False
+        # Print test metrics
+        if print_metrics:
+            test_model.print_metrics()
+
+        mlflow.log_param("dropout_rate", nn_params["dropout_rate"])
+        mlflow.log_metric("MAE", test_model.MAE)
+        mlflow.log_metric("MSE", test_model.MSE)
+        mlflow.log_metric("APE", test_model.APE)
+        mlflow.log_metric("APC", test_model.APC)
+        mlflow.log_metric("RMSE", test_model.RMSE)
+        mlflow.log_metric("rRMSE", test_model.rRMSE)
+
+        mlflow.pytorch.log_model(model, "model")
