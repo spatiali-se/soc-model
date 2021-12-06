d474c82465adb233a8ed534df921f8d84720adaf
nmucke <nmucke@gmail.com>
2021-12-06 21:00:06 +0100

    feat: Added sqrt scaler to target

--- transforms/preprocess.py
+++ transforms/preprocess.py
@@ -26,10 +26,20 @@ def preprocessor(data):
     aspect_col = [cols[15]]
     categorical_col = cols[23:25]
 
+    '''
     target_transformer = Pipeline(
         steps=[  # ("reshape", FunctionTransformer(func=np.reshape, kw_args={"newshape": (-1, 1)})),
             ("minmax_scaler", MinMaxScaler())
         ]
+    )
+    '''
+    target_transformer = Pipeline(
+            steps=[
+                # ("reshape", FunctionTransformer(func=np.reshape, kw_args={
+                # "newshape": (-1, 1)})),
+                ("sqrt_scaler", FunctionTransformer(np.sqrt)),
+                ("minmax_scaler", MinMaxScaler()),
+            ]
     )
 
     # TODO: Implement aspect transformer with cos and sine col output
